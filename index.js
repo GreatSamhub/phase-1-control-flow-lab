fff0accbf107b144d407d3bd0444529cfafb97cf
+Sam Thongo <+samuelthongo73@gmail.com>
2023-06-14 13:10:28 +0300

    Control Flow Lab

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+    return 'This one is on me!'
+  }else if (feet > 2500){
+    return 'No can do.'
+  }else if (feet > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }else{
+    return 'Try another distance'
+  }
 }
+console.log(scuberGreetingForFeet(199));
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
+console.log(ternaryCheckCity('Pittsburgh'))
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank You.'
+    default:      
+      return 'Bye.'
+  } 
 }
+console.log(switchOnCharmFromTip('generous'))
